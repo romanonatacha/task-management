e4c3501440f11f173eed0ea6ae6b8e956ea9ce48
Natacha Romano <natacharomanonr@gmail.com>
2019-11-09 20:47:46 -0300

    Project Setup and Folder Structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrelloList from './TrelloList'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <TrelloList title="test" />
+    </div>
+  );
+}
+
+export default App;
