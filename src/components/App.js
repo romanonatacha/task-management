4e8128c64b7fea4b3483d75113d036df8fa248df
Natacha Romano <natacharomanonr@gmail.com>
2019-11-10 18:28:20 -0300

    Drag and Drop with react-beautiful-dnd

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import TrelloList from './TrelloList'
 import { connect } from 'react-redux'
-import TrelloActionButton from './TrelloActionButton';
+import TrelloActionButton from './TrelloActionButton'
+import { DragDropContext } from 'react-beautiful-dnd'
 
 class App extends Component {
+
+  onDragEnd = () => {
+
+  }
+
   render() {
 
     const { lists } = this.props
 
     return (
-      <div style={styles.body}>
-        <h1>Hello World</h1>
-        <div style={styles.listsContainer}>
-          { lists.map(list => (
-            <TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards} />
-          ))}
-          <TrelloActionButton list />
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div style={styles.body}>
+          <h1>Task Manager</h1>
+          <div style={styles.listsContainer}>
+            { lists.map(list => (
+              <TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards} />
+            ))}
+            <TrelloActionButton list />
+          </div>
         </div>
-      </div>
+      </DragDropContext>
     )
   }
 }
