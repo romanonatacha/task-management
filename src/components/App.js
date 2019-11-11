9c67e11f9c667b7c0d57d259f98f083412865d3c
Natacha Romano <natacharomanonr@gmail.com>
2019-11-11 16:20:11 -0300

    Persisting the Drag and Drop feature

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,31 @@ import TrelloList from './TrelloList'
 import { connect } from 'react-redux'
 import TrelloActionButton from './TrelloActionButton'
 import { DragDropContext } from 'react-beautiful-dnd'
+import { sort } from '../actions'
+import styled from 'styled-components'
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-directioon: row;
+`
 
 class App extends Component {
 
-  onDragEnd = () => {
+  onDragEnd = (result) => {
+    const { destination, source, draggableId } = result
+    if(!destination) {
+      return
+    }
 
+    this.props.dispatch(
+      sort(
+        source.droppableId,
+        destination.droppableId,
+        source.index,
+        destination.index,
+        draggableId
+      )
+    )
   }
 
   render() {
@@ -18,12 +38,12 @@ class App extends Component {
       <DragDropContext onDragEnd={this.onDragEnd}>
         <div style={styles.body}>
           <h1>Task Manager</h1>
-          <div style={styles.listsContainer}>
+          <ListContainer>
             { lists.map(list => (
               <TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards} />
             ))}
             <TrelloActionButton list />
-          </div>
+          </ListContainer>
         </div>
       </DragDropContext>
     )
@@ -33,10 +53,6 @@ class App extends Component {
 const styles = {
   body: {
     fontFamily: 'Roboto'
-  },
-  listsContainer: {
-    display: 'flex',
-    flexDirection: 'row'
   }
 }
 
