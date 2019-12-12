aa9829626571853456a7e1c3e984f63f790310fc
Natacha Romano <natacha.romano@valtech.com>
2019-12-12 18:27:15 -0300

    Updates

---
+++ precache-manifest.ad5cfeb6d47e87af14d85be5d19c6900.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac925f812760cfbe9473399747a547ea",
+    "url": "/task-management/index.html"
+  },
+  {
+    "revision": "7f255a6e4e3dcbb317d7",
+    "url": "/task-management/static/css/main.ba169534.chunk.css"
+  },
+  {
+    "revision": "dc434450d4565abee883",
+    "url": "/task-management/static/js/2.dd0661c0.chunk.js"
+  },
+  {
+    "revision": "7f255a6e4e3dcbb317d7",
+    "url": "/task-management/static/js/main.ec144170.chunk.js"
+  },
+  {
+    "revision": "f78a07b361d60643dd34",
+    "url": "/task-management/static/js/runtime-main.445d7e75.js"
+  }
+]);
