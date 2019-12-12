1549a2bd5ba684f01096d1c5f2b34d966ef3ba8d
Natacha Romano <natacha.romano@valtech.com>
2019-12-12 18:26:40 -0300

    fix conflict

--- src/components/App.js
+++ src/components/App.js
@@ -38,11 +38,7 @@ class App extends Component {
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
         <div style={styles.body}>
-<<<<<<< HEAD
-          <h1>Task Manager</h1>
-=======
           <h1 style={{color: 'white'}}>Task Manager</h1>
->>>>>>> 5bf6e106e0f5b4753e3c92459dbf11b31d3da1cb
           <ListContainer>
             { lists.map(list => (
               <TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards} />
